c9831d6a28a1a3b530e98c67ef7ad520997d308a
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2024-06-19 18:35:18 -0300

    update password

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,5 +4,6 @@ const UsersController = require("../controllers/UsersController");
 const usersRoutes = Router();
 const usersController = new UsersController();
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
