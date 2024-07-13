fd1550e85c71ab117ea33715a2ea8bc0678a7cd0
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2024-07-13 12:28:28 -0300

    index tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const movieNotesRouter = require("./movie_notes.routes");
+const movieTagsRouter = require("./movie_tags.routes");
 
 const routes = Router();
 routes.use("/users", usersRouter);
 routes.use("/movie-notes", movieNotesRouter);
+routes.use("/movie-tags", movieTagsRouter);
 
 module.exports = routes;
---
+++ src/routes/movie_tags.routes.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const MovieTagsController = require("../controllers/MovieTagsController");
+
+const movieTagsRoutes = Router();
+const movieTagsController = new MovieTagsController();
+movieTagsRoutes.get("/:user_id", movieTagsController.index);
+
+module.exports = movieTagsRoutes;
